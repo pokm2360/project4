aa9a1c1a52a16539c825b532f465520e1545b370
'pokm' <'por0632@naver.com'>
2024-04-01 09:28:09 +0900

    4/1 수업 4장마무리

---
+++ src/loop/quiz10.java
@@ -0,0 +1,21 @@
+package loop;
+
+public class quiz10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 중첩반복문을 사용해서 직각삼각형을 그리기
+	
+		
+			for (int i = 1; i <= 5; i++) {
+				for(int j = 1; j<=i; j++) {
+					System.out.print("*");					
+				}
+				System.out.println("");
+			}
+		
+	
+}
+	
+}
